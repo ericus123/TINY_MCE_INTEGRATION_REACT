e20567e439518c8e1484acbe993f78037e409fd0
ericus123 <amaniericus@gmail.com>
2021-05-13 17:01:29 +0200

    integrate TinyMCE in react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleEditorChange = (content, editor) => {
+    localStorage.setItem("content", content);
+  };
+
+  render() {
+    return (
+      <div className="text-editor">
+        <Editor
+          apiKey={process.env.REACT_APP_YOUR_API_KEY}
+          initialValue="<p>Hi, I'm your rich text editor</p>"
+          init={{
+            height: 500,
+            menubar: true,
+            plugins: [
+              "advlist autolink lists link image charmap print preview anchor",
+              "searchreplace visualblocks code fullscreen",
+              "insertdatetime media table paste code help wordcount",
+            ],
+            toolbar:
+              "undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent | removeformat | help",
+          }}
+          onEditorChange={this.handleEditorChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
